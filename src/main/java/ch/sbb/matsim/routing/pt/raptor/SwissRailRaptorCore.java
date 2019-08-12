2175ef2ea6a6147aacf9e2e7276ffc403c15c4fc
leich <leich@vsp.tu-berlin.de>
2019-08-12 17:57:42 +0200

    bug fix related to pt trips without pt legs (only access+egress walk)
    previously of a non_network_walk -> pt interaction -> non_network_walk
    trip the first non_network_walk leg was skipped, giving a wrong
    distance, however total travel time was right. This was due to
    uncommenting if (pes.size() == 2 && pes.get(0).isTransfer &&
    pes.get(1).isTransfer) { ... } above

--- src/main/java/ch/sbb/matsim/routing/pt/raptor/SwissRailRaptorCore.java
+++ src/main/java/ch/sbb/matsim/routing/pt/raptor/SwissRailRaptorCore.java
@@ -836,7 +836,13 @@ public class SwissRailRaptorCore {
                 boolean differentFromTo = (fromStop == null || toStop == null) || (fromStop != toStop);
                 // do not create a transfer-leg if we stay at the same stop facility
                 if (differentFromTo) {
-                    if (i == peCount - 2 && !isIntermodal(pes.get(i+1).initialStop)) {
+                	// add (peCount > 2 || peCount == 2 && !pes.get(0).isTransfer) && to catch case of only access and egress 
+                	// legs without a real leg in between which was previously caught above by 
+                	// pes.size() == 2 && pes.get(0).isTransfer && pes.get(1).isTransfer
+                	//
+                	// in case of peCount < 2 there should be no effect, because peCount-2 < 0 and i will be 0, so i!=peCount - 2
+                	// TODO check
+                    if ((peCount > 2 || peCount == 2 && !pes.get(0).isTransfer) && i == peCount - 2 && !isIntermodal(pes.get(i+1).initialStop)) {
                         // the second last element is a transfer, skip it so it gets merged into the egress_walk
                         // but it can only be merged if it is not intermodal...
                         continue;
